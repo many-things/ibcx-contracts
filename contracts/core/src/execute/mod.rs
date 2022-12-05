9b3bf79b70da960a2d7b0b781c302c4cc37cf112
frostornge <frostornge@gmail.com>
2022-12-05 19:38:39 +0900

    add test for mint & fix logic error

--- contracts/core/src/execute/mod.rs
+++ contracts/core/src/execute/mod.rs
@@ -20,7 +20,7 @@ pub fn mint(
     PAUSED
         .load(deps.storage)?
         .refresh(deps.storage, &env)?
-        .assert_paused()?;
+        .assert_not_paused()?;
 
     // validate!
     let receiver = receiver
@@ -65,7 +65,7 @@ pub fn burn(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, Cont
     PAUSED
         .load(deps.storage)?
         .refresh(deps.storage, &env)?
-        .assert_paused()?;
+        .assert_not_paused()?;
 
     let mut token = TOKEN.load(deps.storage)?;
     let received = cw_utils::must_pay(&info, &token.denom)?;
@@ -90,4 +90,137 @@ pub fn burn(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, Cont
         ]);
 
     Ok(resp)
+}
+
+#[cfg(test)]
+mod test {
+    use cosmwasm_std::{
+        coin,
+        testing::{mock_dependencies_with_balances, mock_env, mock_info},
+        Addr, Storage, SubMsg,
+    };
+
+    use super::*;
+    use crate::state::{PauseInfo, Token, ASSETS, PAUSED};
+
+    fn stop(storage: &mut dyn Storage, now: u64) {
+        PAUSED
+            .save(
+                storage,
+                &PauseInfo {
+                    paused: true,
+                    expires_at: Some(now + 1000),
+                },
+            )
+            .unwrap()
+    }
+
+    fn resume(storage: &mut dyn Storage, now: u64) {
+        PAUSED
+            .save(
+                storage,
+                &PauseInfo {
+                    paused: true,
+                    expires_at: Some(now - 1000),
+                },
+            )
+            .unwrap()
+    }
+
+    fn register_assets(storage: &mut dyn Storage, assets: &[(&str, u128)]) {
+        for (denom, unit) in assets {
+            ASSETS
+                .save(storage, denom.to_string(), &Uint128::from(*unit))
+                .unwrap();
+        }
+    }
+
+    #[test]
+    fn test_mint() {
+        let alice = Addr::unchecked("alice");
+        let bob = Addr::unchecked("bob");
+
+        let max = 10000000000000000;
+        let balances = vec![coin(max, "uaaa"), coin(max, "ubbb"), coin(max, "uccc")];
+        let mut deps = mock_dependencies_with_balances(&[
+            (alice.as_str(), balances.as_slice()),
+            (bob.as_str(), balances.as_slice()),
+        ]);
+        let env = mock_env();
+        let now = env.block.time.seconds();
+
+        let base = 1000000;
+
+        TOKEN
+            .save(
+                deps.as_mut().storage,
+                &Token {
+                    denom: "ibcx".to_string(),
+                    decimal: 6,
+                    reserve_denom: "uosmo".to_string(),
+                    total_supply: Uint128::zero(),
+                },
+            )
+            .unwrap();
+        register_assets(
+            deps.as_mut().storage,
+            &[("uaaa", base), ("ubbb", 10 * base), ("uccc", 100 * base)], // 1 10 100
+        );
+
+        stop(deps.as_mut().storage, now);
+
+        assert!(matches!(
+            mint(
+                deps.as_mut(),
+                env.clone(),
+                mock_info(alice.as_str(), &[]),
+                Uint128::new(base),
+                Some(bob.into_string()),
+            )
+            .unwrap_err(),
+            ContractError::Paused {}
+        ));
+
+        resume(deps.as_mut().storage, now);
+
+        let resp = mint(
+            deps.as_mut(),
+            env.clone(),
+            mock_info(
+                alice.as_str(),
+                &[
+                    coin(2 * base, "uaaa"),
+                    coin(20 * base, "ubbb"),
+                    coin(200 * base, "uccc"),
+                ],
+            ),
+            Uint128::new(base),
+            Some(alice.clone().into_string()),
+        )
+        .unwrap();
+
+        let token = TOKEN.load(deps.as_ref().storage).unwrap();
+        assert_eq!(token.total_supply, Uint128::new(base));
+        assert_eq!(
+            resp.messages,
+            vec![
+                SubMsg::new(MsgMint {
+                    sender: env.contract.address.to_string(),
+                    amount: Some(coin(base, "ibcx").into()),
+                }),
+                SubMsg::new(BankMsg::Send {
+                    to_address: alice.to_string(),
+                    amount: vec![
+                        coin(base, "uaaa"),
+                        coin(10 * base, "ubbb"),
+                        coin(100 * base, "uccc"),
+                        coin(base, "ibcx"),
+                    ],
+                }),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_burn() {}
 }
