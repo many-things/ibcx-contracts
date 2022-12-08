c4c54d50d150ea4690fa3f54d5b40f9e21ab9c20
frostornge <frostornge@gmail.com>
2022-12-08 16:19:18 +0900

    feat: add mock handler fn to handle rebalance msg

--- contracts/core/src/execute/rebalance.rs
+++ contracts/core/src/execute/rebalance.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
+use cosmwasm_std::{Coin, DepsMut, Env, MessageInfo, Response, Uint128};
 use ibc_interface::core::RebalanceMsg;
 
 use crate::error::ContractError;
@@ -16,8 +16,33 @@ pub fn handle_msg(
             manager,
             deflation,
             inflation,
-        } => todo!(),
-        Trade { denom, amount } => todo!(),
-        Finalize {} => todo!(),
+        } => init(deps, env, info, manager, deflation, inflation),
+        Trade { denom, amount } => trade(deps, env, info, denom, amount),
+        Finalize {} => finalize(deps, env, info),
     }
+}
+
+fn init(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    manager: String,
+    deflation: Vec<Coin>,
+    inflation: Vec<Coin>,
+) -> Result<Response, ContractError> {
+    Ok(Default::default())
+}
+
+fn trade(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    denom: String,
+    amount: Uint128,
+) -> Result<Response, ContractError> {
+    Ok(Default::default())
+}
+
+fn finalize(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, ContractError> {
+    Ok(Default::default())
 }
