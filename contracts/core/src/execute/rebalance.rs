f8a79b77d01c260bad09c6e2420fb734060f2322
frostornge <frostornge@gmail.com>
2022-12-08 17:31:09 +0900

    feat: fill init fn & validation

--- contracts/core/src/execute/rebalance.rs
+++ contracts/core/src/execute/rebalance.rs
@@ -1,7 +1,10 @@
-use cosmwasm_std::{Coin, DepsMut, Env, MessageInfo, Response, Uint128};
+use cosmwasm_std::{attr, Coin, DepsMut, Env, MessageInfo, Response, Uint128};
 use ibc_interface::core::RebalanceMsg;
 
-use crate::error::ContractError;
+use crate::{
+    error::ContractError,
+    state::{get_assets, Rebalance, GOV, LATEST_REBALANCE_ID, REBALANCES},
+};
 
 pub fn handle_msg(
     deps: DepsMut,
@@ -22,15 +25,57 @@ pub fn handle_msg(
     }
 }
 
+fn check_duplication(x: Vec<Coin>, y: Vec<Coin>) -> Result<(), ContractError> {
+    let mut y = y.into_iter();
+    let f = x
+        .into_iter()
+        .filter(|xc| y.any(|yc| yc.denom == xc.denom))
+        .collect::<Vec<_>>();
+    if !f.is_empty() {
+        return Err(ContractError::InvalidArgument(format!(
+            "duplicated coin: {:?}",
+            f
+        )));
+    }
+
+    Ok(())
+}
+
 fn init(
     deps: DepsMut,
-    env: Env,
+    _env: Env,
     info: MessageInfo,
     manager: String,
     deflation: Vec<Coin>,
     inflation: Vec<Coin>,
 ) -> Result<Response, ContractError> {
-    Ok(Default::default())
+    if info.sender != GOV.load(deps.storage)? {
+        return Err(ContractError::Unauthorized {});
+    }
+
+    let rebalance_id = LATEST_REBALANCE_ID.load(deps.storage)?;
+    LATEST_REBALANCE_ID.save(deps.storage, &(rebalance_id + 1))?;
+
+    check_duplication(deflation.clone(), inflation.clone())?;
+
+    let assets = get_assets(deps.storage)?;
+    let rebalance = Rebalance {
+        manager: deps.api.addr_validate(&manager)?,
+        snapshot: assets,
+        deflation,
+        inflation,
+        finalized: false,
+    };
+    REBALANCES.save(deps.storage, rebalance_id, &rebalance)?;
+
+    let resp = Response::new().add_attributes(vec![
+        attr("action", "rebalance_init"),
+        attr("executor", info.sender),
+        attr("manager", manager),
+        attr("rebalance_id", rebalance_id.to_string()),
+    ]);
+
+    Ok(resp)
 }
 
 fn trade(
