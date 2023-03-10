831eb275fce6821676f85e4741899ed5720e9dea
byeongsu-hong <hong@byeongsu.dev>
2023-03-10 23:01:28 +0900

    test(core): fee overflow

--- contracts/core/src/state/mod.rs
+++ contracts/core/src/state/mod.rs
@@ -122,9 +122,41 @@ impl PauseInfo {
 
 #[cfg(test)]
 mod test {
+    use std::str::FromStr;
+
     use cosmwasm_std::{testing::mock_env, MemoryStorage};
 
     use super::*;
+
+    #[test]
+    fn test_fee_overflow() {
+        let units = vec![
+            ("uusd".to_string(), Decimal::from_ratio(10u128, 1u128)),
+            ("ukrw".to_string(), Decimal::from_ratio(15u128, 1u128)),
+            ("ujpy".to_string(), Decimal::from_ratio(20u128, 1u128)),
+        ];
+
+        let fee = Fee {
+            collector: Addr::unchecked("collector"),
+            mint: None,
+            burn: None,
+            // secondly rate
+            // ex) APY %0.15 = 1 - (1 + 0.0015)^(1 / (86400 * 365)) = 0.000000000047529
+            stream: Some(Decimal::from_str("0.000000000047529").unwrap()),
+            stream_collected: Units::new(),
+            stream_last_collected_at: 0,
+        };
+
+        // 1 day
+        let day = 86400;
+        fee.calculate_streaming_fee(units.clone(), day).unwrap();
+        // 1 month
+        let month = 30 * day;
+        fee.calculate_streaming_fee(units.clone(), month).unwrap();
+        // 1 year
+        let year = 365 * day;
+        fee.calculate_streaming_fee(units, year).unwrap();
+    }
 
     #[test]
     fn test_refresh() {
