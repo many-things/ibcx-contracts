c99a9f320d1694e21687ba1059746722c1c6678c
byeongsu-hong <hong@byeongsu.dev>
2023-03-24 05:30:33 +0000

    refactor: move merkle verifier to separate file

--- contracts/airdrop/src/lib.rs
+++ contracts/airdrop/src/lib.rs
@@ -5,42 +5,9 @@ use sha2::Digest;
 pub mod contract;
 pub mod error;
 pub mod execute;
+pub mod merkle;
 pub mod query;
 pub mod state;
-
-// verify merkle proof (from https://github.com/cosmwasm/cw-tokens/blob/master/contracts/cw20-merkle-airdrop/src/contract.rs)
-pub fn verify_merkle_proof(
-    root: &str,
-    proof: Vec<String>,
-    claim_proof: &str,
-    amount: Uint128,
-) -> Result<(), ContractError> {
-    let user_input = format!("{claim_proof}:{amount}");
-
-    let hash = sha2::Sha256::digest(user_input.as_bytes())
-        .as_slice()
-        .try_into()
-        .map_err(|_| ContractError::WrongLength {})?;
-
-    let hash = proof.into_iter().try_fold(hash, |hash, p| {
-        let mut proof_buf = [0; 32];
-        hex::decode_to_slice(p, &mut proof_buf)?;
-        let mut hashes = [hash, proof_buf];
-        hashes.sort_unstable();
-        sha2::Sha256::digest(hashes.concat())
-            .as_slice()
-            .try_into()
-            .map_err(|_| ContractError::WrongLength {})
-    })?;
-
-    let mut root_buf: [u8; 32] = [0; 32];
-    hex::decode_to_slice(root, &mut root_buf)?;
-    if root_buf != hash {
-        return Err(ContractError::InvalidProof {});
-    }
-
-    Ok(())
-}
 
 pub const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
---
+++ contracts/airdrop/src/merkle.rs
@@ -0,0 +1,36 @@
+use crate::error::ContractError;
+use cosmwasm_std::Uint128;
+
+// verify merkle proof (from https://github.com/cosmwasm/cw-tokens/blob/master/contracts/cw20-merkle-airdrop/src/contract.rs)
+pub fn verify_merkle_proof(
+    root: &str,
+    proof: Vec<String>,
+    claim_proof: &str,
+    amount: Uint128,
+) -> Result<(), ContractError> {
+    let user_input = format!("{claim_proof}:{amount}");
+
+    let hash = sha2::Sha256::digest(user_input.as_bytes())
+        .as_slice()
+        .try_into()
+        .map_err(|_| ContractError::WrongLength {})?;
+
+    let hash = proof.into_iter().try_fold(hash, |hash, p| {
+        let mut proof_buf = [0; 32];
+        hex::decode_to_slice(p, &mut proof_buf)?;
+        let mut hashes = [hash, proof_buf];
+        hashes.sort_unstable();
+        sha2::Sha256::digest(hashes.concat())
+            .as_slice()
+            .try_into()
+            .map_err(|_| ContractError::WrongLength {})
+    })?;
+
+    let mut root_buf: [u8; 32] = [0; 32];
+    hex::decode_to_slice(root, &mut root_buf)?;
+    if root_buf != hash {
+        return Err(ContractError::InvalidProof {});
+    }
+
+    Ok(())
+}
