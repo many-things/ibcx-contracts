cb9e82a41906b4945054fcaa975ad76b4842f6b3
byeongsu-hong <hong@byeongsu.dev>
2023-03-27 17:52:23 +0900

    test: close

--- contracts/airdrop/src/execute/close.rs
+++ contracts/airdrop/src/execute/close.rs
@@ -93,3 +93,105 @@ fn close_bearer(
 
     Ok(Response::new().add_message(send_msg).add_attributes(attrs))
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{
+        testing::{
+            mock_dependencies_with_balances, mock_env, mock_info, MockApi, MockQuerier, MockStorage,
+        },
+        Addr, OwnedDeps,
+    };
+    use ibcx_interface::airdrop::{AirdropId, AirdropType, InstantiateMsg};
+
+    use crate::{
+        contract::instantiate,
+        execute::{
+            close::close_event,
+            tests::{
+                mock_bearer_airdrop, mock_open_airdrop, normalize_amount, register_airdrop,
+                Balances,
+            },
+        },
+    };
+
+    use super::close;
+
+    fn setup(airdrop_type: AirdropType) -> OwnedDeps<MockStorage, MockApi, MockQuerier> {
+        let env = mock_env();
+
+        let initial_balances: Balances = &[];
+
+        let mut deps = mock_dependencies_with_balances(initial_balances);
+
+        instantiate(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("creator", &[]),
+            InstantiateMsg {},
+        )
+        .unwrap();
+
+        match airdrop_type {
+            AirdropType::Open => {
+                let mock_airdrop = mock_open_airdrop(None, env.block.height);
+                register_airdrop(deps.as_mut(), env, mock_airdrop.into(), None);
+            }
+            AirdropType::Bearer => {
+                let (mock_airdrop, mock_airdrop_sign) = mock_bearer_airdrop(None, env.block.height);
+                register_airdrop(
+                    deps.as_mut(),
+                    env,
+                    mock_airdrop.into(),
+                    Some(mock_airdrop_sign),
+                );
+            }
+        }
+
+        deps
+    }
+
+    #[test]
+    fn test_close_open() {
+        let mut deps = setup(AirdropType::Open);
+
+        let close_resp = close(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("tester", &[]),
+            AirdropId::id(0),
+        )
+        .unwrap();
+        assert_eq!(
+            close_resp.attributes,
+            close_event(
+                Addr::unchecked("tester"),
+                AirdropType::Open,
+                0,
+                normalize_amount(0.01)
+            )
+        );
+    }
+
+    #[test]
+    fn test_close_bearer() {
+        let mut deps = setup(AirdropType::Bearer);
+
+        let close_resp = close(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("tester", &[]),
+            AirdropId::id(0),
+        )
+        .unwrap();
+        assert_eq!(
+            close_resp.attributes,
+            close_event(
+                Addr::unchecked("tester"),
+                AirdropType::Bearer,
+                0,
+                normalize_amount(0.1)
+            )
+        );
+    }
+}
