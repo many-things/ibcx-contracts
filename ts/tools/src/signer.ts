49d0d9e9be2cc4b7cfe207e212745cd841e82940
byeongsu-hong <hong@byeongsu.dev>
2023-03-27 16:29:48 +0900

    feat: string signer

--- ts/tools/src/signer.ts
+++ ts/tools/src/signer.ts
@@ -1,32 +1,46 @@
 import { toBech32 } from "@cosmjs/encoding";
 import { Hash, Mnemonic, PrivKeySecp256k1 } from "@keplr-wallet/crypto";
 
-const [, , MNEMONIC] = process.argv;
+const [, , MNEMONIC, TARGET] = process.argv;
 
-const phrase =
-  MNEMONIC ||
-  "notice oak worry limit wrap speak medal online prefer cluster roof addict wrist behave treat actual wasp year salad speed social layer crew genius";
+if (!MNEMONIC) {
+  throw Error("Please provide a mnemonic phrase");
+}
 
-console.log(`Input mnemonic: ${phrase}`);
+if (!TARGET) {
+  console.log(
+    "INFO: No target is provided. The address will be used as the target."
+  );
+}
 
 async function main() {
   const bzToStr = (bz: Uint8Array) => Buffer.from(bz).toString("hex");
 
-  const privKeyBz = Mnemonic.generateWalletFromMnemonic(phrase);
+  const privKeyBz = Mnemonic.generateWalletFromMnemonic(MNEMONIC);
   const privKey = new PrivKeySecp256k1(privKeyBz);
   const pubKey = privKey.getPubKey();
   const addr = toBech32("osmo", pubKey.getAddress());
 
-  const digest = Hash.sha256(Buffer.from(addr));
+  const digest = Hash.sha256(Buffer.from(TARGET || addr));
   const sign = privKey.signDigest32(digest);
 
-  console.log("pubkey    :", bzToStr(pubKey.toBytes()));
-  console.log("privkey   :", bzToStr(privKey.toBytes()));
-  console.log("address   :", addr);
-  console.log("signature :", bzToStr(sign));
+  console.log(
+    JSON.stringify(
+      {
+        pubKey: bzToStr(pubKey.toBytes()),
+        privKey: bzToStr(privKey.toBytes()),
+        address: addr,
+        signature: bzToStr(sign),
+      },
+      null,
+      2
+    )
+  );
 
   const verified = pubKey.verifyDigest32(digest, sign);
-  console.log("verified  :", verified);
+  if (!verified) {
+    throw Error("verification failed");
+  }
 }
 
 main().catch(console.error);
