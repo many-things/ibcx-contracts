9a6794c31524d3feca63d19544bf5e46f16e4a74
byeongsu-hong <hong@byeongsu.dev>
2023-03-31 04:47:55 +0900

    test rebalance::finalize

--- contracts/core/src/execute/rebalance/finalize.rs
+++ contracts/core/src/execute/rebalance/finalize.rs
@@ -35,7 +35,7 @@ pub fn finalize(deps: DepsMut, env: Env, info: MessageInfo) -> StdResult<Respons
     for (denom, target_unit) in rebalance.deflation {
         let (_, current_unit) = index_units.get_key(&denom).unwrap();
 
-        if current_unit < &target_unit {
+        if &target_unit < current_unit {
             return Err(
                 RebalanceError::unable_to_finalize("deflation condition did not met").into(),
             );
@@ -53,7 +53,7 @@ pub fn finalize(deps: DepsMut, env: Env, info: MessageInfo) -> StdResult<Respons
 
     // response
     let attrs = vec![
-        attr("method", "finalize"),
+        attr("method", "rebalance::finalize"),
         attr("executor", info.sender),
         attr("finalized_at", env.block.height.to_string()),
     ];
@@ -63,7 +63,158 @@ pub fn finalize(deps: DepsMut, env: Env, info: MessageInfo) -> StdResult<Respons
 
 #[cfg(test)]
 mod tests {
+    use cosmwasm_std::{
+        attr,
+        testing::{mock_env, mock_info, MockStorage},
+        Addr,
+    };
+
+    use crate::{
+        error::{ContractError, RebalanceError},
+        state::{Fee, Rebalance, StreamingFee, Units, FEE, INDEX_UNITS, REBALANCE, RESERVE_UNITS},
+        test::mock_dependencies,
+    };
+
+    use super::{finalize, unfreeze_streaming_fee};
 
     #[test]
-    fn test_finalize() {}
+    fn test_unfreeze_streaming_fee() {
+        let mut storage = MockStorage::new();
+
+        FEE.save(
+            &mut storage,
+            &Fee {
+                streaming_fee: None,
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        unfreeze_streaming_fee(&mut storage, 30).unwrap();
+        assert!(FEE.load(&storage).unwrap().streaming_fee.is_none());
+
+        FEE.save(
+            &mut storage,
+            &Fee {
+                streaming_fee: Some(StreamingFee {
+                    freeze: true,
+                    last_collected_at: 10,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        unfreeze_streaming_fee(&mut storage, 12345).unwrap();
+
+        let streaming_fee = FEE.load(&storage).unwrap().streaming_fee.unwrap();
+        assert!(!streaming_fee.freeze);
+        assert_eq!(streaming_fee.last_collected_at, 12345);
+    }
+
+    #[test]
+    fn test_finalize() {
+        let env = mock_env();
+        let mut deps = mock_dependencies();
+
+        FEE.save(deps.as_mut().storage, &Fee::default()).unwrap();
+
+        let cases = [
+            // sender = random, manager = none
+            (
+                "user",
+                vec![("uatom", "0.89")].into(),
+                vec![("uatom", "0.0")].into(),
+                Some(Rebalance {
+                    manager: None,
+                    deflation: vec![("uatom", "0.9")].into(),
+                    inflation: Units::default(),
+                }),
+                Ok(vec![
+                    attr("method", "rebalance::finalize"),
+                    attr("executor", "user"),
+                    attr("finalized_at", env.block.height.to_string()),
+                ]),
+            ),
+            // sender = random, manager = some
+            (
+                "user",
+                vec![("uatom", "0.89")].into(),
+                vec![("uatom", "0.0")].into(),
+                Some(Rebalance {
+                    manager: Some(Addr::unchecked("manager")),
+                    deflation: vec![("uatom", "0.9")].into(),
+                    inflation: Units::default(),
+                }),
+                Err(ContractError::Unauthorized),
+            ),
+            // sender = manager, manager = some
+            (
+                "manager",
+                vec![("uatom", "0.89")].into(),
+                vec![("uatom", "0.0")].into(),
+                Some(Rebalance {
+                    manager: Some(Addr::unchecked("manager")),
+                    deflation: vec![("uatom", "0.9")].into(),
+                    inflation: Units::default(),
+                }),
+                Ok(vec![
+                    attr("method", "rebalance::finalize"),
+                    attr("executor", "manager"),
+                    attr("finalized_at", env.block.height.to_string()),
+                ]),
+            ),
+            // index_unit > deflation
+            (
+                "manager",
+                vec![("uatom", "0.91")].into(),
+                vec![("uatom", "0.0")].into(),
+                Some(Rebalance {
+                    manager: Some(Addr::unchecked("manager")),
+                    deflation: vec![("uatom", "0.9")].into(),
+                    inflation: Units::default(),
+                }),
+                Err(RebalanceError::unable_to_finalize("deflation condition did not met").into()),
+            ),
+            // reserve_units != zero
+            (
+                "manager",
+                vec![("uatom", "0.89")].into(),
+                vec![("uatom", "0.01")].into(),
+                Some(Rebalance {
+                    manager: Some(Addr::unchecked("manager")),
+                    deflation: vec![("uatom", "0.9")].into(),
+                    inflation: Units::default(),
+                }),
+                Err(RebalanceError::unable_to_finalize("inflation condition did not met").into()),
+            ),
+        ];
+
+        for (sender, index_units, reserve_units, rebalance, expected) in cases {
+            INDEX_UNITS
+                .save(deps.as_mut().storage, &index_units)
+                .unwrap();
+
+            RESERVE_UNITS
+                .save(deps.as_mut().storage, &reserve_units)
+                .unwrap();
+
+            REBALANCE.remove(deps.as_mut().storage);
+            if let Some(rebalance) = rebalance {
+                REBALANCE.save(deps.as_mut().storage, &rebalance).unwrap();
+            }
+
+            let res = finalize(deps.as_mut(), env.clone(), mock_info(sender, &[]));
+            assert_eq!(res.map(|v| v.attributes), expected);
+
+            if expected.is_ok() {
+                assert!(REBALANCE.may_load(deps.as_ref().storage).unwrap().is_none());
+                assert!(RESERVE_UNITS
+                    .may_load(deps.as_ref().storage)
+                    .unwrap()
+                    .is_none());
+            }
+        }
+    }
 }
