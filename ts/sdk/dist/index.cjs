2bca9b9d34fdd274a5bd5c4dcc19d26d96a432b6
byeongsu-hong <hong@byeongsu.dev>
2023-04-26 01:27:27 +0900

    0.1.4

--- ts/sdk/dist/index.cjs
+++ ts/sdk/dist/index.cjs
@@ -1,5 +1,5 @@
 /*!
- * @many-things/ibcx-contracts-sdk v0.1.3
+ * @many-things/ibcx-contracts-sdk v0.1.4
  * (c) frostornge <frostornge@gmail.com>
  * Released under the MIT OR Apache-2.0 License.
  */
