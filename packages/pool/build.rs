0ba753e854d92be6a0a6f1d76e72b1c41e267834
byeongsu-hong <hong@byeongsu.dev>
2023-10-04 14:11:45 +0000

    finish build script

--- packages/pool/build.rs
+++ packages/pool/build.rs
@@ -1,8 +1,15 @@
-use std::process::Command;
+use std::{fs, process::Command};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    let resp =
+        reqwest::blocking::get("https://lcd.osmosis.zone/osmosis/poolmanager/v1beta1/all-pools")?
+            .text()?;
+
+    fs::write("tests/testdata/all-pools.json", resp)?;
+
     Command::new("node")
         .arg("tests/testdata/conv.js")
-        .output()
-        .unwrap();
+        .output()?;
+
+    Ok(())
 }
